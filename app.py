beb4a5f4771ae98c5adedd30939ba09a8e1dd5b7
ctscmtdemo <ctscmtdemo@gmail.com>
2024-12-02 18:01:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 from io import BytesIO
-from azure.storage.blob import BlobServiceClient, ContainerClient
 from dotenv import load_dotenv
 import os
 from difflib import get_close_matches  # For better matching
